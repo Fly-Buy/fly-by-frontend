d4a1f5a7e33dd4cf64ab5d28fe9e66b724acad71
Alex Bennett <alexandergraybennett@gmail.com>
2016-02-15 15:42:39 -0700

    adding piechart

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -18,7 +18,7 @@ angular.module('flyBuyApp')
       console.log('flight data: ', data.data.chart_data);
       that.flightData = data;
       that.data = data.data.chart_data;
-      // barChart = graphs.barChart(data.data.chart_data);
+
     });
 
     this.flightInfo = {
@@ -65,10 +65,10 @@ angular.module('flyBuyApp')
           height: 450,
           width: 600,
           margin : {
-              top: 20,
-              right: 20,
-              bottom: 60,
-              left: 70
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: 70
           },
           x: function(d){ return d.label; },
           y: function(d){ return d.value; },
@@ -87,19 +87,27 @@ angular.module('flyBuyApp')
       }
     };
 
-    // this.data = [{
-    //   key: "Cumulative Return",
-    //   values: [
-    //       { "label" : "A" , "value" : -29.765957771107 },
-    //       { "label" : "B" , "value" : 0 },
-    //       { "label" : "B" , "value" : 1 },
-    //       { "label" : "C" , "value" : 32.807804682612 },
-    //       { "label" : "D" , "value" : 196.45946739256 },
-    //       { "label" : "E" , "value" : 0.19434030906893 },
-    //       { "label" : "F" , "value" : -98.079782601442 },
-    //       { "label" : "G" , "value" : -13.925743130903 },
-    //       { "label" : "H" , "value" : -5.1387322875705 }
-    //       ]
-    //   }];
 
-  });
+    this.pieChart = {
+      chart: {
+        type: 'pieChart',
+        height: 500,
+        x: function(d){return d.key;},
+        y: function(d){return d.y;},
+        showLabels: true,
+        duration: 500,
+        labelThreshold: 0.01,
+        labelSunbeamLayout: true,
+        legend: {
+          margin: {
+              top: 5,
+              right: 35,
+              bottom: 5,
+              left: 0
+          }
+        }
+      }
+    }
+
+
+});
